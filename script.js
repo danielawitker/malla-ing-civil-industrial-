fc7bacb667a2606956cea4cffe5c7322f4cd7442
danielawitker <danielawitkerr@gmail.com>
2025-07-13 21:22:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,68 @@
-// Prerrequisitos ajustados para coincidir con IDs reales del HTML
+// Prerrequisitos ajustados según el PDF oficial de la malla
 const prerequisitos = {
+  // Año 1
   "algebra_lineal": ["algebra_intro", "programacion", "bases_ingenieria"],
   "calculo1": ["algebra_intro", "bases_ingenieria"],
   "mecanica": ["algebra_intro"],
-  "biologia_micro": ["bases_ingenieria"],
   "economia": ["algebra_intro"],
-  "optativo1": ["algebra_intro"],
-  "optativo2": ["algebra_intro"],
-  "minor1": ["optativo1"],
+  "biologia_micro": ["bases_ingenieria"],
   "teologia2": ["teologia1"],
-  "teologia3": ["teologia2"],
+  "optativo1": ["algebra_intro"], // Optativo Arte/Lit abre Minor1
   "etica": ["antropologia"],
+
+  // Año 2
   "ecuaciones": ["algebra_lineal", "calculo1", "programacion"],
   "calculo2": ["calculo1", "programacion"],
   "mecanica_ondas": ["mecanica"],
+  "minor1": ["optativo1"],
   "prob_estad": ["calculo2"],
   "electromagnetismo": ["calculo2", "mecanica_ondas"],
   "termodinamica": ["calculo2", "mecanica_ondas", "quimica"],
   "estatica": ["calculo1", "mecanica_ondas"],
+  "teologia3": ["teologia2"],
+
+  // Año 3
   "filosofia_ciencia": ["etica"],
   "minor2": ["minor1"],
   "minor3": ["minor2"],
   "optimizacion": ["calculo2"],
+  "metodos_estad": ["prob_estad"],
+  "contabilidad": [],
+  "concentracion1": [],
+
+  // Año 4
   "modelos_estoc": ["optimizacion", "prob_estad"],
   "prog_matematica": ["optimizacion", "prob_estad"],
+  "bases_datos": ["programacion"],
+  "microeconomia": ["economia"],
+  "concentracion2": ["concentracion1"],
+  "pensamiento_diseno": ["concentracion2"],
   "simulacion": ["modelos_estoc", "metodos_estad"],
   "econometria": ["metodos_estad"],
   "finanzas1": ["contabilidad"],
-  "concentracion2": ["concentracion1"],
-  "concentracion3": ["concentracion2"],
-  "pensamiento_diseno": ["concentracion2"],
+
+  // Año 5
   "diseno_eval_proyectos": ["simulacion"],
   "gestion_operaciones": ["econometria", "prog_matematica"],
   "finanzas2": ["finanzas1"],
+  "macro": ["economia"],
   "gestion_estrategica": ["diseno_eval_proyectos"],
   "gestion_personas": ["diseno_eval_proyectos"],
-  "logistica": ["gestion_operaciones"],
+
+  // Año 5 Sem IX
+  "org_industrial": ["microeconomia"],
   "marketing": ["econometria"],
+  "logistica": ["gestion_operaciones"],
+  "mencion1": ["macro"],
+  "electivo_esp1": ["macro"],
+
+  // Año 5 Sem X
   "mencion2": ["org_industrial"],
   "electivo_esp2": ["org_industrial"],
   "seminario": ["org_industrial"],
+  "proyecto1": ["org_industrial"],
+
+  // Año 6
   "mencion3": ["gestion_estrategica"],
   "electivo_esp3": ["gestion_estrategica"],
   "proyecto2": ["proyecto1"]
